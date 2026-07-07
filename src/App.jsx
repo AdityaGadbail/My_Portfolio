63ba66b95650a40dd588fc7939e1c6bc37905f86
Aditya Gadbail <163659268+AdityaGadbail@users.noreply.github.com>
2026-07-07 19:06:55 +0530

    Add AnimatedCursor component to App

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer';
+import AnimatedCursor from './components/AnimatedCursor';
 
 function App() {
   
 
   return (
     <div>  
+      <AnimatedCursor />
       <ToastContainer/>
     <Navbar/> 
     <Home />    
